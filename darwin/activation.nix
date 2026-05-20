8ac580c75aac0ed35afa33510ca429cba3a23bcf
Sreyas Sabbani <sreyassabbani@gmail.com>
2026-05-19 20:34:34 -0400

    chore: configure Finder folder views

--- darwin/activation.nix
+++ darwin/activation.nix
@@ -1,9 +1,45 @@
 { config, pkgs, ... }:
+let
+  user = config.system.primaryUser;
+  home = "/Users/${user}";
+in
 {
   system.activationScripts.defaultBrowser.text = ''
     # See available names:
     #   ${pkgs.defaultbrowser}/bin/defaultbrowser -l
     ${pkgs.defaultbrowser}/bin/defaultbrowser "Zen Browser"
+  '';
+
+  system.activationScripts.finderFolderViews.text = ''
+    echo "configuring Finder folder views..." >&2
+
+    if [ -d "${home}/workflow" ]; then
+      find "${home}/workflow" -type d -exec sudo -u ${user} osascript -e '
+          on run argv
+            tell application "Finder"
+              set targetFolder to POSIX file (item 1 of argv) as alias
+              open targetFolder
+              set current view of Finder window 1 to column view
+              close Finder window 1
+            end tell
+          end run
+        ' {} \; >/dev/null
+    fi
+
+    if [ -d "${home}/Downloads" ]; then
+      sudo -u ${user} osascript -e '
+        tell application "Finder"
+          set targetFolder to POSIX file "'"${home}/Downloads"'" as alias
+          open targetFolder
+          set current view of Finder window 1 to list view
+          tell list view options of Finder window 1
+            set sort column to modification date column
+            set sort direction of column modification date column to reversed
+          end tell
+          close Finder window 1
+        end tell
+      ' >/dev/null
+    fi
   '';
 
   system.activationScripts.applications.text =
