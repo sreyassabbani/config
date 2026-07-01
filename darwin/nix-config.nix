9b8c15d3233cd72be8d0b28537d63fd8596a9a39
Sreyas Sabbani <sreyassabbani@gmail.com>
2026-07-01 14:55:06 -0400

    chore(darwin/nix): automate store cleanup
    - add launchd jobs for weekly `nix-collect-garbage --delete-older-than 14d` and `nix store optimise`
    
    - use explicit Determinate Nix command paths because nix-darwin `nix.gc.automatic` requires `nix.enable`
    
    - write daemon logs to `/var/log/nix-gc.log` and `/var/log/nix-store-optimise.log` for troubleshooting

--- darwin/nix-config.nix
+++ darwin/nix-config.nix
@@ -2,6 +2,38 @@
 {
   nix.enable = false;
 
+  launchd.daemons = {
+    nix-gc.serviceConfig = {
+      ProgramArguments = [
+        "/nix/var/nix/profiles/default/bin/nix-collect-garbage"
+        "--delete-older-than"
+        "14d"
+      ];
+      StartCalendarInterval = {
+        Weekday = 0;
+        Hour = 3;
+        Minute = 15;
+      };
+      StandardOutPath = "/var/log/nix-gc.log";
+      StandardErrorPath = "/var/log/nix-gc.log";
+    };
+
+    nix-store-optimise.serviceConfig = {
+      ProgramArguments = [
+        "/nix/var/nix/profiles/default/bin/nix"
+        "store"
+        "optimise"
+      ];
+      StartCalendarInterval = {
+        Weekday = 0;
+        Hour = 4;
+        Minute = 15;
+      };
+      StandardOutPath = "/var/log/nix-store-optimise.log";
+      StandardErrorPath = "/var/log/nix-store-optimise.log";
+    };
+  };
+
   determinateNix.customSettings = {
     eval-cores = 0;
 
